32aa87783747832e7f2b532e1a88d355fc63e86d
magn01ia <84883260+magn01ia@users.noreply.github.com>
2023-02-22 08:22:30 +0900

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,101 @@
+var map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://gsi-cyberjapan.github.io/gsivectortile-mapbox-gl-js/pale.json', // style URL
+    center: [140.084556, 36.104611], // starting position [lng, lat]
+    zoom: 7, // starting zoom
+    maxZoom: 4, // min zoom
+    maxZoom: 17 // max zoom
+    });  
+    map.on('load', function () {
+    
+  //------------------------------------------------------------------下記水戸ベクトル
+  //ベクトルタイル表示、オーバーズーミングしていない？
+    map.addSource("mito", {
+        type: "vector",
+        tiles: [
+            "https://magn01ia.github.io/fudevt/mito/{z}/{x}/{y}.pbf"
+        ],
+        minzoom: 10,
+        maxzoom: 14, //ここでソースのズームレベルを指定してやらないとオーバーズーミングが効かない
+    });
+    map.addLayer({
+        id: "fude",
+        type: "fill",
+        source: "mito",
+        "source-layer": "mito",
+        "minzoom": 9,
+        "maxzoom": 22,
+        paint: {
+            "fill-color": "#FF00FF",
+            "fill-opacity": 0.3,
+            "fill-outline-color": "white"
+        }
+    });
+});
+
+map.addControl(new maplibregl.FullscreenControl());
+
+//PMTilesプラグインの追加
+let protocol = new pmtiles.Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
+const p = new pmtiles.PMTiles("https://x.optgeo.org/a.pmtiles")
+protocol.add(p);
+map.on('load', () => {
+      // 法務省地図XMLベクトルタイル追加
+      map.addSource("pmtiles", {
+        type: "vector",
+        url: "pmtiles://https://x.optgeo.org/a.pmtiles",
+        attribution: '© <a href="https://github.com/amx-project">法務省地図XMLアダプトプロジェクト</a>'
+    });
+//代表点のアウト
+    // map.addLayer({
+        //     "id": "daihyo",
+        //     "source": "pmtiles",
+        //     "source-layer": "daihyo",
+        //     "type": "circle",
+        //     'paint': {
+            //         'circle-color': '00FFCC',
+            //         'circle-blur': 0.5,
+            //         'circle-radius': 3
+            //     }
+            // }); 
+//代表点の中身 
+map.addLayer({
+    "id": "daihyo2",
+    "source": "pmtiles",
+    "source-layer": "daihyo",
+    "type": "circle",
+    'paint': {
+        'circle-color': '#00CCCC',
+        //'circle-radius': 1
+        'circle-radius': 2
+    }
+});
+  
+//筆ポリゴンのライン
+map.addLayer({
+    "id": "fude-line",
+    "source": "pmtiles",
+    "source-layer": "fude",
+    "type": "line",
+    "paint": {
+        'line-color': '#009966',
+        'line-width': 2,
+    }
+});
+  
+//筆ポリゴン
+map.addLayer({
+    "id": "fude-poligon",
+    "source": "pmtiles",
+    "source-layer": "fude",
+    "type": "fill",
+    "paint": {
+        'fill-color': '#00FFCC',
+        'fill-opacity': 0.25,
+    }
+});
+});
+    
+  
+  
